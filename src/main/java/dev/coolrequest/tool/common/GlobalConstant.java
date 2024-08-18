9df38ef3faef2f461e6afa1de9d3d30e49ba1ce4
lhstack <lhstack@foxmail.com>
2024-08-18 22:08:32 +0800

    优化groovy在编辑器中打开逻辑,增加编辑器监听器,修复当切换窗口导致groovy在编辑器中组件丢失的问题

--- src/main/java/dev/coolrequest/tool/common/GlobalConstant.java
+++ src/main/java/dev/coolrequest/tool/common/GlobalConstant.java
@@ -3,11 +3,13 @@ package dev.coolrequest.tool.common;
 import com.intellij.openapi.util.Key;
 import com.intellij.psi.PsiFile;
 import com.intellij.ui.components.JBTextArea;
+import com.intellij.util.messages.MessageBusConnection;
 import dev.coolrequest.tool.components.MultiLanguageTextField;
 import dev.coolrequest.tool.views.coder.Coder;
 
 import javax.swing.*;
 import java.util.List;
+import java.util.Set;
 
 public interface GlobalConstant {
 
@@ -61,5 +63,27 @@ public interface GlobalConstant {
      * 缓存target comboBox
      */
     Key<DefaultComboBoxModel<String>> CODER_TARGET_BOX_MODEL_KEY = Key.create("CoderTargetBoxModel");
+
+    /**
+     * 缓存项目状态,存在则已经处理
+     */
+    Key<Set<String>> CODER_STATE_CACHE = Key.create("CoderStateCache");
+
+
+    /**
+     * 是否初始化CoderGroovyFileEditorListener监听器的key
+     */
+    String CODER_GROOVY_FILE_EDITOR_LISTEN_INIT_KEY = "CoderGroovyFileEditorListenInitKey";
+
+
+    /**
+     * 全局消息总线
+     */
+    Key<MessageBusConnection> MESSAGE_BUS_CONNECTION_KEY = Key.create("MessageBusConnection");
+
+    /**
+     * Groovy 在编辑器中打开的top组件
+     */
+    Key<JComponent> CODER_GROOVY_FILE_EDITOR_TOP_COMPONENT = Key.create("CoderGroovyFileEditorTopComponent");
 
 }
